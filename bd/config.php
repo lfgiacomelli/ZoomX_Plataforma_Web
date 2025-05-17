52c64f4afdc71c01ee73583b067b65ab9ae127a4
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-17 19:18:11 -0300

    Delete bd/config.php

--- bd/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('LOCAL_PATH', 'http://localhost/tcc/');
-define('HOST', 'localhost');
-define('PORT', '3306');
-define('DBNAME', 'tcc');
-define('USER', 'root');
-define('PASSWORD', '');
-define('CHARSET', 'utf8');
