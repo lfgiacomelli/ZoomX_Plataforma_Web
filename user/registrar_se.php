a5e6c0ad3845579fc844354f31a11f434ddba94f
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-18 16:05:37 -0300

    [fix] class input password

--- user/registrar_se.php
+++ user/registrar_se.php
@@ -39,7 +39,7 @@ unset($_SESSION['erro_email']);
             <div class="form-group">
                 <label for="senha">Senha</label>
                 <div class="password-container">
-                    <input type="password" name="senha" id="senha" placeholder="Insira sua Senha" autocomplete="new-password" required>
+                    <input type="password" name="senha" id="senha" class="form-control" placeholder="Insira sua Senha" autocomplete="new-password" required>
 
                     <i class="fas fa-eye password-toggle" id="togglePassword"></i>
                 </div>
