a0843bd9b6ad629143b76432bacfd736f2948e1f
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-18 20:39:39 -0300

    [feat] session.save_path

--- components/verifica_sessao.php
+++ components/verifica_sessao.php
@@ -1,12 +1,15 @@
 <?php
+require_once __DIR__ . '/../routes/config.php'; 
+
+ini_set('session.save_path', '/tmp');
+
 session_start();
 
 if (
     !isset($_SESSION['logado099']) ||
     !isset($_SESSION['tipo']) ||
-    $_SESSION['tipo'] !== 'usuario'
+    $_SESSION['tipo'] !== 'atendente'
 ) {
-       header('Location: ' . BASE_URL .'user/login.php');
-
+    header('Location: ' . BASE_URL . 'user/login.php');
     exit;
 }
--- components/verifica_sessao_admin.php
+++ components/verifica_sessao_admin.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../routes/config.php'; 
 
+ini_set('session.save_path', '/tmp');
+
 session_start();
 
 if (
@@ -8,6 +10,6 @@ if (
     !isset($_SESSION['tipo']) ||
     $_SESSION['tipo'] !== 'atendente'
 ) {
-    header('Location: ' . BASE_URL .'admin/login.php');
+    header('Location: ' . BASE_URL . 'admin/login.php');
     exit;
 }
