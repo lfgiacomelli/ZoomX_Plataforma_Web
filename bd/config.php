db7ff4cb28d9642040bebad8597255ed55ad38d3
Giacomelli <133823261+lfgiacomelli@users.noreply.github.com>
2025-07-19 00:40:53 -0300

    [fix] db info

--- bd/config.php
+++ bd/config.php
@@ -1,13 +1,13 @@
 <?php
 define('LOCAL_PATH', getenv('LOCAL_PATH') ?: 'https://zoomx.onrender.com/');
 
-$databaseUrl = getenv('DATABASE_URL') ?: 'postgresql://giacomelli_devs:NyO6nehZ5tWBFopexVOJrpvCalF0y2ZS@dpg-d18bupggjchc73ep07vg-a.oregon-postgres.render.com:5432/zoomx_tcc_fx7z';
+$databaseUrl = getenv('DATABASE_URL') ?: 'postgresql://giacomelli_devs:fqIvi8jKnQWe9hsFRUHZWMsEigT6TOwD@dpg-d1tgnl3ipnbc73cafgf0-a.oregon-postgres.render.com/zoomx_database';
 
 $dbparts = parse_url($databaseUrl);
 
 define('HOST', $dbparts['host'] ?? 'localhost');
 define('PORT', $dbparts['port'] ?? '5432');
-define('DBNAME', ltrim($dbparts['path'], '/') ?? 'zoomx_tcc_fx7z');
+define('DBNAME', ltrim($dbparts['path'], '/') ?? 'zoomx_database');
 define('USER', $dbparts['user'] ?? 'giacomelli_devs');
-define('PASSWORD', $dbparts['pass'] ?? 'NyO6nehZ5tWBFopexVOJrpvCalF0y2ZS');
+define('PASSWORD', $dbparts['pass'] ?? 'fqIvi8jKnQWe9hsFRUHZWMsEigT6TOwD');
 define('CHARSET', 'utf8');
